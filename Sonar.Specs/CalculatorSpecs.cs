9a637774cd1194b9267983f3e9440a44ddb6dc60
Andreas Danek <a.danek@live.com>
2020-09-05 00:00:39 +0200

    Added basic project setup

---
+++ Sonar.Specs/CalculatorSpecs.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="CalculatorSpecs.cs" company="World-Direct">
+// Copyright (c) World-Direct. All rights reserved.
+// </copyright>
+
+namespace Sonar.Specs
+{
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Contains the specification for the <see cref="Calculator"/> class.
+    /// </summary>
+    public class CalculatorSpecs
+    {
+        /// <summary>
+        /// The calcutltor can add two numbers.
+        /// </summary>
+        [Fact]
+        public void TheCalculatorCanAdd2Numbers()
+        {
+            var cut = new Calculator();
+
+            var result = cut.Add(1, 2);
+
+            result.Should().Be(3);
+        }
+    }
+}
---
+++ Sonar/Calculator.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Calculator.cs" company="World-Direct">
+// Copyright (c) World-Direct. All rights reserved.
+// </copyright>
+
+namespace Sonar
+{
+    /// <summary>
+    /// Provides the functionaltiy to do math.
+    /// </summary>
+    public class Calculator
+    {
+
+        /// <summary>
+        /// Adds the two numbers
+        /// </summary>
+        /// <param name="v1">The first number.</param>
+        /// <param name="v2">The second number.</param>
+        /// <returns>The sum of the two numbers.</returns>
+        public object Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
