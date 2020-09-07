e15283b9937de820cc6d28ca22cf5e773bddafa4
Andreas Danek <a.danek@live.com>
2020-09-07 23:33:48 +0200

    Added docs project

---
+++ Sonar.Docs/articles/details-1.md
@@ -0,0 +1,13 @@
+# Details 1
+
+Some detail description.
+
+## Section 1
+
+some content
+
+## Section 2
+
+### Sub section
+
+Some more text
---
+++ Sonar/Helper/Printer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sonar.Helper
+{
+    public class Printer
+    {
+        public void Print()
+        {
+
+        }
+    }
+}
